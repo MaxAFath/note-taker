e96d6a391657d59174ae8d39982df42f51808083
Max Fathauer <maxfathauer@gmail.com>
2022-07-13 22:18:43 -0400

    fixed req req

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
-app.post('/api/notes', (req, req) => {
+app.post('/api/notes', (req, res) => {
     let id = makeId();
     let newNote = {
         id: id,
@@ -56,9 +56,6 @@ app.delete('api/notes:id', (req, res) => {
 
 })
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-})
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
